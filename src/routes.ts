91c4aeedc307668c18899d76630a1e738a324ee6
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2025-04-15 21:46:57 -0300

    ecommerce

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,10 @@ import multer from 'multer';
 import uploadConfig from './config/multer';
 import { checkRole } from "./middlewares/checkRole";
 import { isAuthenticatedEcommerce } from "./middlewares/isAuthenticatedEcommerce";
+import { body } from "express-validator/lib/middlewares/validation-chain-builders";
 
+// --- USUARIOS E-COMMERCE --- //
+import { UserEcommerceCreateController } from "./controllers/users/UserEcommerceCreateController";
 
 // --- COLORS --- //
 import { ThemeController } from "./controllers/configuration_blog/theme_setting/ThemeController";
@@ -11,9 +14,8 @@ import { ThemeController } from "./controllers/configuration_blog/theme_setting/
 // --- TEMPLATES DE EMAILS
 import EmailTemplateController from "./controllers/templates_emails/EmailTemplateController";
 
-// --- USUARIOS E-COMMERCE --- //
-import { UserEcommerceCreateController } from "./controllers/users/UserEcommerceCreateController";
-import { body } from "express-validator/lib/middlewares/validation-chain-builders";
+
+
 
 
 
@@ -26,7 +28,7 @@ const controller = new ThemeController();
 // --- USUARIOS E-COMMERCE --- //
 router.post('/user/ecommerce/create', upload_image.single('file'), new UserEcommerceCreateController().handle);
 
-// -- ROUTES THEME SETTINGS --
+// -- COLORS --
 router.get('/theme', controller.getTheme);
 router.put(
     '/theme',
@@ -50,19 +52,14 @@ router.put(
 // --- TEMPLATES DE EMAILS --- //
 // Rota para listar todos os templates
 router.get('/email-templates', EmailTemplateController.getAll);
-
 // Rota para obter um template por ID
 router.get('/email-templates/:id', EmailTemplateController.getById);
-
 // Rota para criar um novo template
 router.post('/email-templates', EmailTemplateController.create);
-
 // Rota para atualizar um template existente
 router.put('/email-templates/:id', EmailTemplateController.update);
-
 // Rota para excluir um template
 router.delete('/email-templates/:id', EmailTemplateController.delete);
-
 // Rota para renderizar um template (útil para previews/testes de renderização)
 router.post('/email-templates/:id/render', EmailTemplateController.render);
 
