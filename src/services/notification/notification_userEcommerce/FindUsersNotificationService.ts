407d6f53aa0f944a66e190c2de5aaa3b082b0111
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2025-04-29 17:30:52 -0300

    ecommerce

--- src/services/notification/notification_userEcommerce/FindUsersNotificationService.ts
+++ src/services/notification/notification_userEcommerce/FindUsersNotificationService.ts
@@ -1,6 +1,6 @@
 import moment from "moment";
 import prismaClient from "../../../prisma";
-import { Prisma } from "@prisma/client";
+import { Prisma, NotificationType } from "@prisma/client"; // Importe o enum NotificationType
 
 class FindUsersNotificationService {
     async execute(
@@ -15,10 +15,19 @@ class FindUsersNotificationService {
     ) {
         const skip = (page - 1) * limit;
 
+        // Função para obter os valores do enum que correspondem à pesquisa
+        const getMatchingNotificationTypes = (searchTerm: string): NotificationType[] => {
+            const searchLower = searchTerm.toLowerCase();
+            return Object.values(NotificationType).filter(type =>
+                type.toLowerCase().includes(searchLower)
+            );
+        };
+
         const whereClause: Prisma.NotificationUserEcommerceWhereInput = {
             ...(search ? {
                 OR: [
-                    { message: { contains: search, mode: Prisma.QueryMode.insensitive } }
+                    // Filtra pelos tipos de notificação que correspondem à pesquisa
+                    { type: { in: getMatchingNotificationTypes(search) } }
                 ]
             } : {}),
             ...(startDate && endDate ? {
