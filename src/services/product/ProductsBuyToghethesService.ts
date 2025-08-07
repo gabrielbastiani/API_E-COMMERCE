a980d1fea3e8cb2b854e0a9fba0cd4e186063cd5
Gabriel Campos de Bastiani <gabriel.bastiani@hotmail.com.br>
2025-08-07 17:34:02 -0300

    ecommerce

--- src/services/product/ProductsBuyToghethesService.ts
+++ src/services/product/ProductsBuyToghethesService.ts
@@ -8,7 +8,8 @@ class ProductsBuyToghethesService {
     async execute({ product_id }: ProductRequest) {
         const productData = await prismaClient.product.findUnique({
             where: {
-                id: product_id
+                id: product_id,
+                status: "DISPONIVEL"
             },
             include: {
                 images: true
